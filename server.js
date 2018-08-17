999f01101fb217360da35e4e5ffa3666b182da48
hp <gajuajayvijay@gmail.com>
2018-08-17 20:15:57 +0530

    setup github and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
@@ -36,7 +37,7 @@ app.post('/api/chk',(req,res)=>
 });
 
 
-const port = 5000;
+
 
 app.listen(port,()=>console.log(`Server is up and running at ${port}`));
 
